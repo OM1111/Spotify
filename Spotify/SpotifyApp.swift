9b5ef3d9f4ea83d05da97cfd6499928e532b29f0
OMBHA <95213510+OMBHA@users.noreply.github.com>
2024-05-04 08:04:35 +0530

    Initial Commit

---
+++ Spotify/SpotifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpotifyApp.swift
+//  Spotify
+//
+//  Created by iOS Cat on 04/05/24.
+//
+
+import SwiftUI
+
+@main
+struct SpotifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
